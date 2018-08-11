e016f90ff2a490276f7ad85fde1a17e63f679d41
Chi Chi <malachijb28@gmail.com>
2018-08-11 10:08:07 -0400

    Change to different trajectory

--- src/Main/TrajectorySetup.java
+++ src/Main/TrajectorySetup.java
@@ -18,7 +18,7 @@ public class TrajectorySetup {
 	boolean backward;
 	public int counter;
 	public double xRobotOut1, yRobotOut1, xRobotOut2, yRobotOut2;
-	public int posTraj = 3;
+	public int posTraj = 6;
 	Waypoint[] points;
 	
 	
